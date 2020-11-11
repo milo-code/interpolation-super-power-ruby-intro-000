2fb9700e64f2c582da0e28ab68cf06eea7221964
Hemmy <c.hemant.c@gmail.com>
2020-11-11 18:32:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def #display_rainbow (colorinput)
+  puts "R: #{colorinput[0]}, O:#{colorinput[1]}, Y:#{colorinput[2]}, G:#{colorinput[3]},B:#{color[4]}, I:#{color[5]}, V:#{color[6]}"
+end 
